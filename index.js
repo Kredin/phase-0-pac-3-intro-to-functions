1b41a6b859076aed86d4a85d1bc281b9808af7ec
Blade <bladeprojectz@gmail.com>
2024-02-01 13:29:34 +0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Follow along with the examples here
+
+
+function sayHello() {
+    console.log(`Hello ${naem}`);
+}
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+}
+
+sayHello();
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+const naem = "fred"
